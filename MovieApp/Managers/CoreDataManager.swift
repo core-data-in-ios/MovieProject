398f92d0aa8534a617fef49b90d8c041e8467abc
Chris Hand <cj.hand@gmail.com>
2022-09-18 05:31:20 -0700

    Initialize the coredatamodel

--- MovieApp/Managers/CoreDataManager.swift
+++ MovieApp/Managers/CoreDataManager.swift
@@ -11,4 +11,17 @@ import CoreData
 class CoreDataManager {
     // This is added code from the lecture
    
+    let persistantContainer: NSPersistentContainer
+    
+    static let shared = CoreDataManager() //A singleton instance
+    
+    private init() {
+        persistantContainer = NSPersistentContainer(name: "MovieAppModel") // the name of the entity
+        persistantContainer.loadPersistentStores { (description, error) in
+            if let error = error {
+                fatalError("failed to initialize Core Data \(error.localizedDescription)")
+            }
+        }
+    }
+    
 }
